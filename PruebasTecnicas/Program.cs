04ff98a1f4c1f4b0840d0610f670cc395e3d5576
Leandro Muñoz <47321348+leandromunioz@users.noreply.github.com>
2024-04-23 17:21:31 -0300

    Add files via upload

---
+++ PruebasTecnicas/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace PruebasTecnicas
+{
+    using System;
+    using System.Numerics;
+
+    class Program
+    {
+        static void Main()
+        {
+            int[] S = { 7, 9, -4, 8, 11, 1, 2 };
+            int v = 9;
+
+            var resultado = EncontrarDosEnterosQueSumanV(S, v);
+            if (resultado != null)
+            {
+                Console.WriteLine($"Los números que suman {v} son {resultado.Value.Item1} y {resultado.Value.Item2}.");
+            }
+            else
+            {
+                Console.WriteLine($"No existen números en el array que sumen {v}. Por favor, ingrese un número válido.");
+            }
+        }
+
+        public static (int, int)? EncontrarDosEnterosQueSumanV(int[] S, int v)
+        {
+            Dictionary<int, int> complementosNecesarios = new Dictionary<int, int>();
+            foreach (int num in S)
+            {
+                if (complementosNecesarios.ContainsKey(num))
+                {
+                    return (num, v - num);
+                }
+                int complemento = v - num;
+                complementosNecesarios[complemento] = num;
+            }
+            return null;
+        }
+
+    }
+    
+
+}
+
+
